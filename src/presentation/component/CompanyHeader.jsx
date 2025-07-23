f95642bc5407170d09ab043822ca9dba9976c493
jh-salman <jhsalman53@gmail.com>
2025-07-23 21:16:46 +0600

    final version in ready

--- src/presentation/component/CompanyHeader.jsx
+++ src/presentation/component/CompanyHeader.jsx
@@ -23,7 +23,7 @@ function CompanyHeader() {
                 fontFamily: 'Segoe UI, Tahoma, Geneva, Verdana, sans-serif',
                 clipPath: 'polygon(0 0, 100% 0, 93.5% 100%, 0% 100%)', // ✅ Cor
             }}>
-                <div>
+                <div onClick={() => window.location.href = "https://lv3.com/?gad_source=1&gad_campaignid=21854289107&gbraid=0AAAAABj5NaAqTlQbtNBMozoKOOa-fhfSr&gclid=EAIaIQobChMIv7jAjqLTjgMVN1R_AB3VZgLaEAAYASAAEgKh9fD_BwE"} style={{ cursor: 'pointer' }}>
                     <img width={190} src='./levellogo.png' />
                 </div>
                 <p style={{
